d5216c308f78a22de8afd56ec2eab1abd2fd7290
sharov_vitalii <fly44088@gmail.com>
2023-12-20 23:46:22 +0300

    1

---
+++ tests/simple/test_fail.py
@@ -0,0 +1,15 @@
+def test_fail():
+    assert False
+
+
+def test_fail3():
+    assert False
+
+def test_fail4():
+    assert False
+
+def test_fail5():
+    assert False
+
+def test_fail6():
+    assert False
